fad8e174fe6c66bda4cd94bc2b8c7e7c4c77b936
allEyezOnCode <alexandredewilde2001@gmail.com>
2023-08-11 16:14:17 +0200

    add problem

--- kattis/borders.py
+++ kattis/borders.py
@@ -3,62 +3,88 @@ n, m = map(int, input().split())
 
 grid = [list(input()) for _ in range(n)]
 
-vis = [[-1] * m for _ in range(n)]
+comps = [[-1] * m for _ in range(n)]
 
 def dfs(x, y, id_):
     stack = [(x,y)]
-    vis[x][y] = id_
+    comps[x][y] = id_
     while stack:
         x, y, = stack.pop()
         for xx, yy in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
-            if 0 <= xx < n and 0 <= yy < m and vis[xx][yy] == -1 and grid[xx][yy] == grid[x][y]:
-                vis[xx][yy] = id_
+            if 0 <= xx < n and 0 <= yy < m and comps[xx][yy] == -1 and grid[xx][yy] == grid[x][y]:
+                comps[xx][yy] = id_
                 stack.append((xx,yy))
 
 nb_comps = 0
 for i in range(n):
     for j in range(m):
-        if vis[i][j] == -1:
+        if comps[i][j] == -1:
             dfs(i, j, nb_comps)
             nb_comps += 1
-g = [[] for _ in range(nb_comps)]
 
-done = [[False]*m for _ in range(m)]
-def dfs(a, b):
-    stack = [(a,b)]
-    done[a][b] = True
-    added = set([vis[a][b]])
-    while stack:
-        x, y = stack.pop()
-        for xx, yy in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
-            if 0 <= xx < n and 0 <= yy < m and done[xx][yy] == False and grid[xx][yy] == grid[x][y]:
-                done[xx][yy] = True
-                stack.append((xx,yy))
-            elif 0 <= xx < n and 0 <= yy < m and vis[xx][yy] not in added:
-                g[vis[a][b]].append(vis[xx][yy])
-                added.add(vis[xx][yy])
+is_border = [False] * nb_comps
 
 for i in range(n):
+    is_border[comps[i][-0]] |= True
+    is_border[comps[i][-1]] |= True
+
+for j in range(m):
+    is_border[comps[-0][j]] |= True
+    is_border[comps[-1][j]] |= True
+
+ans = sum(is_border)
+
+v1 = 0
+v2 = 0
+ids = [-1] * nb_comps
+for i in range(n):
     for j in range(m):
-        if not done[i][j]:
-            dfs(i, j)
-match = [-1] * nb_comps
-vis = [False] * nb_comps
+        if ids[comps[i][j]] != -1 or is_border[comps[i][j]]:
+            continue
+        if grid[i][j] == "1":
+            ids[comps[i][j]] = v1
+            v1 += 1
+        else:
+            ids[comps[i][j]] = v2
+            v2 += 1
+        
+g = [[] for _ in range(v1)]
+added = set()
+vis = [[False]*m for _ in range(n)]
+def dfs2(x, y):
+    vis[x][y] = True
+    for xx, yy in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
+        if 0 <= xx < n and 0 <= yy < m and vis[xx][yy] == False and grid[xx][yy] == grid[x][y] and not is_border[comps[xx][yy]]:
+            dfs2(xx, yy)
+        elif 0 <= xx < n and 0 <= yy < m and vis[xx][yy] == False and not is_border[comps[xx][yy]]:
+            if grid[x][y] == "1" and (ids[comps[x][y]], ids[comps[xx][yy]]) not in added:
+                added.add((ids[comps[x][y]], ids[comps[xx][yy]]))
+                g[ids[comps[x][y]]].append(ids[comps[xx][yy]])
+            elif grid[x][y] == "0" and (ids[comps[xx][yy]], ids[comps[x][y]]) not in added:
+                added.add((ids[comps[xx][yy]], ids[comps[x][y]]))
+                g[ids[comps[xx][yy]]].append(ids[comps[x][y]])
+for i in range(n):
+    for j in range(m):
+        if not vis[i][j] and not is_border[comps[i][j]]:
+            dfs2(i, j)
 
-def augment(x):
-    if vis[x]:
+match = [-1] * v2
+vis = [False] * v1
+
+def augment(v):
+    if vis[v]:
         return False
-    vis[x] = True
-    for adj in g[x]:
-        if match[adj] == -1 or augment(match[adj]):
-            match[adj] = x
+    vis[v] = True
+    for u in g[v]:
+        if match[u] == -1 or augment(match[u]):
+            match[u] = v
             return True
-
+        
     return False
 
-ans = 0
-for i in range(nb_comps):
-    for j in range(nb_comps): vis[j] = False
-    if augment(i): ans += 1
-
-print(ans, g, nb_comps, match)
+for i in range(v1):
+    for j in range(v1):
+        vis[j] = False
+    if augment(i):
+        ans += 1
+print(ans)
